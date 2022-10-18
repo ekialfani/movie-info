bbd8e4cd1c9503ba66fe9a09c17696d9557ac444
Eki Alfani <ekialfani15@gmail.com>
2022-10-18 22:36:25 +0800

    add: ESLint for code-style-guide

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style/style.css';
+
+const firstName = 'eki';
+const lastName = 'alfani';
+
+const getName = (firstName, lastName) => {
+  console.log(`my name is: ${firstName} ${lastName}`);
+};
+
+getName(firstName, lastName);
