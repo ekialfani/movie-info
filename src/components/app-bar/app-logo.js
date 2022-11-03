edc1b665115ce1847d7da09ae9457d2f3be88918
Eki Alfani <ekialfani15@gmail.com>
2022-11-04 00:14:59 +0800

    add: shadow dom on app-logo component

--- src/components/app-bar/app-logo.js
+++ src/components/app-bar/app-logo.js
@@ -1,17 +1,89 @@
-import '../../styles/app-bar/app-logo.css';
+/* eslint-disable space-before-blocks */
+/* eslint-disable no-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable require-jsdoc */
 
 class AppLogo extends HTMLElement {
+  constructor(){
+    super();
+    this.shadowDOM = this.attachShadow({mode: 'open'});
+  }
+
   connectedCallback() {
+  	// eslint-disable-next-line no-tabs
+  	// eslint-disable-next-line no-tabs
   	this.render();
   }
 
   render() {
-  	this.innerHTML = (`
+    const newLocal = `
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+
+      div {
+        padding: 20px 8%;
+        display: flex;
+        align-items: center;
+      }
+      
+      .logo {
+        font-family: 'Stick No Bills', sans-serif;
+        color: white;
+        font-size: 3em;
+        font-weight: bold;
+        color: #16a34a;
+      }
+      
+      .logo-text {
+        font-family: 'Signika Negative', sans-serif;
+        margin-left: 5px;
+        font-size: 2em;
+        color: white;
+      }
+      
+      @media screen and (max-width: 1440px){
+        .logo {
+          font-size: 2.5em;
+        }
+      
+        .logo-text {
+          font-size: 1.5em;
+        }
+      }
+      
+      @media screen and (max-width: 992px){
+        div {
+          padding: 20px 5% 0;
+        }
+      }
+      
+      @media screen and (max-width: 768px){
+        div {
+          padding: 20px 20px 0;
+        }
+      }
+      
+      @media screen and (max-width: 576px){
+        .logo {
+          font-size: 2em;
+        }
+      
+        .logo-text {
+          font-size: 1em;
+        }
+      }
+    </style>
+
       <div>
         <span class="logo">M</span>
         <span class="logo-text">movie info</span>
       </div>
-    `);
+    `;
+  	this.shadowDOM.innerHTML = (newLocal);
   }
 }
 
