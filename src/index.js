67dbf702d05b510f737ca01a2f94c4bcff859854
Eki Alfani <ekialfani15@gmail.com>
2022-11-05 00:10:44 +0800

    optimize: webpack bundle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/style.css';
+import './another-module.js';
 import './components/app-bar/app-logo.js';
 import './components/jumbotron/movie-jumbotron.js';
 import './components/categories/up-coming.js';
-// import './components/feature/movie-details.js';
 import main from './views/main.js';
 
 document.addEventListener('DOMContentLoaded', main);
