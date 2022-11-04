fe03e9a705e0c69ba9103acfb14cf8aa4e63eb6e
Eki Alfani <ekialfani15@gmail.com>
2022-11-05 04:14:49 +0800

    add: movie-credits component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './another-module.js';
 import './components/app-bar/app-logo.js';
 import './components/jumbotron/movie-jumbotron.js';
 import './components/categories/up-coming.js';
+import './components/credits/movie-credits.js';
 import main from './views/main.js';
 
 document.addEventListener('DOMContentLoaded', main);
